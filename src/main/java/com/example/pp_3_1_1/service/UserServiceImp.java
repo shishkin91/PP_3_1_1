462cf1c9ea9b50ec4b0ea9e14538c46fade4a609
Vitaly Shishkin <67963461+shishkin91@users.noreply.github.com>
2022-05-31 18:54:27 +0300

    commit 2

--- src/main/java/com/example/pp_3_1_1/service/UserServiceImp.java
+++ src/main/java/com/example/pp_3_1_1/service/UserServiceImp.java
@@ -3,6 +3,7 @@ package com.example.pp_3_1_1.service;
 import com.example.pp_3_1_1.model.User;
 import com.example.pp_3_1_1.dao.UserDao;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,16 +14,18 @@ public class UserServiceImp implements UserService {
     public UserServiceImp(UserDao userDao) {
         this.userDao = userDao;
     }
-
+    @Transactional
     public User findById(Long id) {
         return userDao.findById(id).orElse(null);
     }
     public List<User> findAll() {
         return (List<User>) userDao.findAll();
     }
+    @Transactional
     public User saveUser(User user) {
         return userDao.save(user);
     }
+    @Transactional
     public void deleteById(Long id) {
         userDao.deleteById(id);
     }
